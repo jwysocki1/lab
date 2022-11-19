2796b5ae7e622725e75315ae4155d2a076ac2707
jwysocki1 <jwysocki@unomaha.edu>
2022-11-18 18:44:58 -0600

    Class file.

--- account.py
+++ account.py
@@ -1,25 +1,52 @@
-
+#Class file
 
 class Account:
-    def __init__(self, name, balance=0):
+    '''
+    This class will be used to create accounts which are identified by a name and
+    a balance, the balance is able to change.
+    '''
+    def __init__(self, name: str, balance: int =0) -> None:
+        '''
+        Used to identify an account.
+        :param name: The name of the person who owns the account.
+        :param balance: The balance of the account
+        '''
         self.__name = name
         self.__balance = balance
 
-    def deposit(self, amount):
+    def deposit(self, amount: float or int) -> bool:
+        '''
+        Used to increase an account's balance.
+        :param amount: The amount of money being added to the account's balance.
+        :return: If money is added:  return True; if not: return False;
+        '''
         if amount > 0:
             self.__balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float or int) -> bool:
+        '''
+        Used to decrease an account's balance.
+        :param amount: The amount of money being subtraced from the account's balance.
+        :return: If money is subtracted:  return True; if not: return False;
+        '''
         if amount > 0 and amount < self.__balance:
             self.__balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float or int:
+        '''
+        Used to retrieve an account's balance.
+        :return: an account's balance
+        '''
         return self.__balance
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        Used to retrieve an account owner's name.
+        :return: The account owner's name
+        '''
         return self.__name
